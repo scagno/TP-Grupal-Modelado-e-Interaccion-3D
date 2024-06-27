69e973c21f15dc6f4d21bea4652d438b7f9c723a
NachoAyalaBuira <163532574+NachoAyalaBuira@users.noreply.github.com>
2024-06-27 10:38:08 -0300

    cambios en el codigo, se añadieron nuevos scripts y cambios en la UI

--- Assets/Scripts/Codigo Btns.cs
+++ Assets/Scripts/Codigo Btns.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class comprobantedeprecio : MonoBehaviour
 {
-    int alimentos;
-    public int DineroDisponible;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +13,6 @@ public class comprobantedeprecio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(Btn-Alcanza-y-Sobra) alimentos<DineroDisponible)
-        {
-
-        }
+        
     }
 }
