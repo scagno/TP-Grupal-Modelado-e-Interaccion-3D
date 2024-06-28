accb73d88a20249ca3bc291b24cc5d4e2984a447
tobimmj <166714447+tobimmj@users.noreply.github.com>
2024-06-28 09:35:05 -0300

    rotaciones y decoracion

--- Assets/Scripts/Producto.cs
+++ Assets/Scripts/Producto.cs
@@ -7,4 +7,12 @@ public class Producto : MonoBehaviour
 
     public int precio;
 
+    private float xRotationSpeed = 60f;
+    private float yRotationSpeed = 20f;
+
+    private void Update()
+    {
+        transform.Rotate(transform.up * Time.deltaTime * xRotationSpeed + transform.right * yRotationSpeed * Time.deltaTime + transform.forward / 2 * -yRotationSpeed * Time.deltaTime);
+    }
+
 }
