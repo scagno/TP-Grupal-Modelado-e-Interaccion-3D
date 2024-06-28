025f31f985deeb2e934462d505b90b3c0721f7e6
tobimmj <166714447+tobimmj@users.noreply.github.com>
2024-06-27 22:03:40 -0300

    Script de Sapwnear Alimentos, Producto

--- Assets/Scripts/Producto.cs
+++ Assets/Scripts/Producto.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Producto : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int precio;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Scripts/comprobantedeprecio.cs
+++ Assets/Scripts/comprobantedeprecio.cs
@@ -1,23 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class comprobantedeprecio : MonoBehaviour
 {
     int alimentos;
     public int DineroDisponible;
+    public Text DineroTexto;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DineroDisponible = Random.Range(5, 100);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(Btn-Alcanza-y-Sobra) alimentos<DineroDisponible)
-        {
-
-        }
+        
     }
 }
